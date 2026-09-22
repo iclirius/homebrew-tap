895321d5d3ea114b6e6ea1d29a84de026a579b18
iclirius <iclirius.kain.d.p.s.v9@gmail.com>
2026-09-21 23:10:56 -0600

    brew: update iclirius to v1.0.0-rc.8

--- Formula/iclirius.rb
+++ Formula/iclirius.rb
@@ -1,8 +1,8 @@
 class Iclirius < Formula
   desc "Local-first Iclirius agent runtime and terminal interface"
   homepage "https://github.com/iclirius/iclirius-distribution"
-  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.7/iclirius-1.0.0-rc.7.tar.gz"
-  sha256 "55e50210163c09547137526ed2794d51b2fdb6bbacd3a8be9cadc2414d36dfbc"
+  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.8/iclirius-1.0.0-rc.8.tar.gz"
+  sha256 "ef48cf1470bc7432b4334282fcbe1046fab9113a5aec457a22f18b59117ea640"
   license "MIT"
 
   depends_on "python@3.14"
