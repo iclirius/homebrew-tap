958d0c170333a81a40d38909a01aac2cad87adb3
iclirius <iclirius.kain.d.p.s.v9@gmail.com>
2026-09-23 00:39:35 -0600

    Update Iclirius to 1.0.0-rc.10

--- Formula/iclirius.rb
+++ Formula/iclirius.rb
@@ -1,8 +1,8 @@
 class Iclirius < Formula
   desc "Local-first Iclirius agent runtime and terminal interface"
   homepage "https://github.com/iclirius/iclirius-distribution"
-  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.9/iclirius-1.0.0-rc.9.tar.gz"
-  sha256 "6ab19068ad804e06c6d146c0aed26bed8b613bdd6ddf5078e79a86ddead5343f"
+  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.10/iclirius_openclaw-1.0.0rc10-py3-none-any.whl"
+  sha256 "726fd6962985c7714dff143d5bab7c9ccfd745baa2687cdd172e784998d29eff"
   license "MIT"
 
   depends_on "python@3.14"
@@ -12,7 +12,7 @@ class Iclirius < Formula
     venv = libexec/"venv"
     system formula_opt_bin("python@3.14")/"python3.14", "-m", "venv", venv
     system venv/"bin/pip", "install", "--upgrade", "pip"
-    system venv/"bin/pip", "install", "."
+    system venv/"bin/pip", "install", cached_download
     bin.install_symlink venv/"bin/iclirius"
   end
 
