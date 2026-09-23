7ca068c50352be631068c3c4a98c0233c88b6e30
iclirius <iclirius.kain.d.p.s.v9@gmail.com>
2026-09-23 01:11:09 -0600

    Update Iclirius to RC11 node service fix

--- Formula/iclirius.rb
+++ Formula/iclirius.rb
@@ -1,8 +1,8 @@
 class Iclirius < Formula
   desc "Local-first Iclirius agent runtime and terminal interface"
   homepage "https://github.com/iclirius/iclirius-distribution"
-  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.10/iclirius_openclaw-1.0.0rc10-py3-none-any.whl"
-  sha256 "726fd6962985c7714dff143d5bab7c9ccfd745baa2687cdd172e784998d29eff"
+  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.11/iclirius_openclaw-1.0.0rc11-py3-none-any.whl"
+  sha256 "e50a6ae45001aefe27232506a4f103b865cbea22fb0203e43fe3466312253193"
   license "MIT"
 
   depends_on "python@3.14"
@@ -12,14 +12,14 @@ class Iclirius < Formula
     venv = libexec/"venv"
     system formula_opt_bin("python@3.14")/"python3.14", "-m", "venv", venv
     system venv/"bin/pip", "install", "--upgrade", "pip"
-    wheel = buildpath/"iclirius_openclaw-1.0.0rc10-py3-none-any.whl"
+    wheel = buildpath/"iclirius_openclaw-1.0.0rc11-py3-none-any.whl"
     cp cached_download, wheel
     system venv/"bin/pip", "install", wheel
     bin.install_symlink venv/"bin/iclirius"
   end
 
   test do
-    assert_match "1.0.0-rc.10", shell_output("#{bin}/iclirius --version")
+    assert_match "1.0.0-rc.11", shell_output("#{bin}/iclirius --version")
     system bin/"iclirius", "node", "status", "--json"
   end
 end
