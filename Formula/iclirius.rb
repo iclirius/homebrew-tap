834dd2e0cb26f3ddf9c3400643925b82e366ae0b
iclirius <iclirius.kain.d.p.s.v9@gmail.com>
2026-09-23 07:10:44 -0600

    iclirius 1.0.0-rc.22

--- Formula/iclirius.rb
+++ Formula/iclirius.rb
@@ -1,8 +1,8 @@
 class Iclirius < Formula
   desc "Local-first Iclirius agent runtime and terminal interface"
   homepage "https://github.com/iclirius/iclirius-distribution"
-  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.21/iclirius_openclaw-1.0.0rc21-py3-none-any.whl"
-  sha256 "1d2e82921e9f04bfc77210f147610b242b71d2c62ea6d41117239a422f5af49f"
+  url "https://github.com/iclirius/iclirius-distribution/releases/download/v1.0.0-rc.22/iclirius_openclaw-1.0.0rc22-py3-none-any.whl"
+  sha256 "0dfbaca15f9c22514f9fe1d69423c05a18a5c1f0a9eb65ea4c00e40593fa4f73"
   license "MIT"
 
   depends_on "python@3.14"
@@ -12,7 +12,7 @@ class Iclirius < Formula
     venv = libexec/"venv"
     system formula_opt_bin("python@3.14")/"python3.14", "-m", "venv", venv
     system venv/"bin/pip", "install", "--upgrade", "pip"
-    wheel = buildpath/"iclirius_openclaw-1.0.0rc21-py3-none-any.whl"
+    wheel = buildpath/"iclirius_openclaw-1.0.0rc22-py3-none-any.whl"
     cp cached_download, wheel
     system venv/"bin/pip", "install", wheel
     bin.install_symlink venv/"bin/iclirius"
